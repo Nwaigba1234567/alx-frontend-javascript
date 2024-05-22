83959ce47a1f27e5167b8a85a1d4a698676c73ed
Kate Nwaigba <iamkatty3@gmail.com>
2024-05-22 12:19:01 -0700

    updated code

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,8 @@
 export default function getListStudents() {
-  const createStudent = (id, firstName, location) => ({
-    id,
-    firstName,
-    location,
-  });
-
-  return [
-    createStudent(1, 'Guillaume', 'San Francisco'),
-    createStudent(2, 'James', 'Columbia'),
-    createStudent(5, 'Serena', 'San Francisco'),
+  const result = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+  return result;
 }
